1a9f265fca50c18d49d990e182c9f52f39d54812
Balvis <107074621+bralvis2@users.noreply.github.com>
2022-07-10 15:00:20 -0500

    Added the main planning section

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Variables
+var todoColor = $('.todo-color');
+var rowEl = $('.rows');
+
+// Curent Date and Time in the header
+var currentDay = moment().format('dddd, MMMM Do, h:mm a');
+$('#currentDay').text(currentDay);
+
+// Assigns light-grey color to the To Do column if the time has passed current time
+
+
+// Assigns green color to the TO Do column if the time has not passed.
+
+
+//Assigns red color to the TO Do column of the current hour. 
+
+//
